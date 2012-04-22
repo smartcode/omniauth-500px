133864fe4239648f6d2173a253fc6f05ac4beea7
Matthias Leitner <male@runtastic.com>
2012-04-22 18:07:22 +0200

    bugfix

--- lib/omniauth/strategies/auth500px.rb
+++ lib/omniauth/strategies/auth500px.rb
@@ -6,27 +6,46 @@ module OmniAuth
 
       option :name, '500px'
       option :client_options, {
+        :access_token_path => '/v1/oauth/access_token',
         :authorize_path => '/v1/oauth/authorize',
-        :token_path => '/v1/oauth/access_token',
         :request_token_path => "/v1/oauth/request_token",
         :site => 'https://api.500px.com'
       }
 
       info do
         {
-          :user_id      => raw_info["id"],
-          :first_name   => raw_info["firstname"],
-          :last_name    => raw_info["lastname"],
-          :image        => raw_info["userpic_url"],
-          :url          => raw_info["permalink"],
+          :nickname => user_info['username'],
+          :email => raw_info['email'],
+          :first_name => raw_info['firstname'],
+          :last_name => raw_info['lastname'],
+          :description => user_info['about'],
+          :image => raw_info['userpic_url'],
+        }
+      end
+
+      extra do
+        {
+          :raw_info => raw_info
         }
       end
 
       uid { raw_info["id"] }
+      
+      def request_phase
+        options[:authorize_params] = {:perms => options[:scope]} if options[:scope]
+        super
+      end
 
     
-      def raw_info      
-        @raw_info ||= MultiJson.decode(access_token.get('/v1/oauth/users').body)["user"]
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get('/v1/users.json').body)
+        rescue ::Errno::ETIMEDOUT
+          raise ::Timeout::Error
+      end
+
+
+      def user_info
+        @user_info ||= raw_info.nil? ? {} : raw_info["user"]
       end
     end
   end
